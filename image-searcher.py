63a8bfff12d9d4b518e98f7b3fc8d0d69d86d57f
Neko <57575090+Nekoraru22@users.noreply.github.com>
2020-05-06 20:21:50 +0200

    Update image-searcher.py

--- image-searcher.py
+++ image-searcher.py
@@ -3,7 +3,6 @@ import requests, shutil, bs4, time, string, re, os, sys, nekos, sqlite3
 from colorama import Fore, init
 from bs4 import BeautifulSoup
 from tqdm import tqdm
-from random import randint, uniform, random
 # Ejemplo de URL que da la API: 'https://cdn.nekos.life/lewd/lewd_neko488.jpeg'
 
 init()
@@ -17,19 +16,35 @@ print(Fore.GREEN + ' #              Discord: Neko-san#4592             #')
 print(Fore.GREEN + ' #                                                 #')
 print(Fore.GREEN + ' ###################################################' + Fore.RESET + '\n')
 
+# ------------------------------------ Lists -------------------------------------- #
 mylist = ['tickle', 'classic', 'ngif', 'erofeet', 'erok', 'poke', 'les', 'hololewd', 'lewdk', 'keta', 'feetg', 'nsfw_neko_gif', 'eroyuri', 'kiss', 'kuni', 'tits', 'pussy_jpg', 'cum_jpg', 'pussy', 'lewdkemo', 'slap', 'lewd', 'cum', 'cuddle', 'spank', 'smallboobs', 'Random_hentai_gif', 'avatar', 'fox_girl', 'nsfw_avatar', 'hug', 'gecg', 'boobs', 'pat', 'feet', 'smug', 'kemonomimi', 'solog', 'holo', 'wallpaper', 'bj', 'woof', 'yuri', 'trap', 'anal', 'baka', 'blowjob', 'holoero', 'feed', 'neko', 'gasm', 'hentai', 'futanari', 'ero', 'solo', 'waifu', 'pwankg', 'eron', 'erokemo']
 mylist.sort()
+# ------------------------------------ /Lists -------------------------------------- #
 
-# ------------------------------------ Word Remove --------------------------------------#
+# ------------------------------------ Create folders -------------------------------------- #
+os.makedirs('Downloader/Guest', mode=0o777, exist_ok=True)
+
+def lewdfolder():
+    for names in mylist:
+        os.makedirs('Downloader/' + names, mode=0o777, exist_ok=True)
+    print(Fore.LIGHTGREEN_EX + '[-] Folders created succesfully' + Fore.RESET)
+# ------------------------------------ /Create folders -------------------------------------- #
+
+# ------------------------------------ Word Remove -------------------------------------- #
 def remove_char(string):
     string = list(string)
     string.remove('/')
     return ''.join(string)
-# ------------------------------------ /Word Remove --------------------------------------#
+# ------------------------------------ /Word Remove -------------------------------------- #
 
-# ------------------------------------ Loop as lewd mode --------------------------------------#
+# -------- Rute ----------- #
+rute = 'Downloader/'
+# -------- /Rute ----------- #
+
+# ------------------------------------ Loop as lewd mode -------------------------------------- #
 def download():
     global tag
+    global rute
     link = nekos.img(tag)
     print(Fore.LIGHTMAGENTA_EX + "URL Image: " + Fore.LIGHTCYAN_EX + link + Fore.RESET)
 
@@ -39,17 +54,22 @@ def download():
     print (Fore.LIGHTMAGENTA_EX + 'Image name: ' + Fore.LIGHTCYAN_EX + image_name + Fore.RESET)
 
     resp = requests.get(link, stream=True)
-    local_file = open(image_name, 'wb')
-    resp.raw.decode_content = True
-    shutil.copyfileobj(resp.raw, local_file)
-    del resp
-    # Dowload image file
-    for i in tqdm(range(100), unit=" bits", desc= "Getting File"):
-      print('', end='\r')
-# ------------------------------------ /Loop as lewd mode --------------------------------------#
+    try:
+        local_file = open(rute + tag + '/' + image_name, 'xb')
+        resp.raw.decode_content = True
+        shutil.copyfileobj(resp.raw, local_file)
+        del resp
+        # Dowload image file
+        for i in tqdm(range(100), unit=" bits", desc= "Getting File"):
+            print('', end='\r')
+    except:
+        errors()
+# ------------------------------------ /Loop as lewd mode -------------------------------------- #
 
-# ------------------------------------ Loop as guest mode --------------------------------------#
+# ------------------------------------ Loop as guest mode -------------------------------------- #
 def download2():
+    global tag
+    global rute
     link = nekos.cat()
     print(Fore.LIGHTMAGENTA_EX + "URL Image: " + Fore.LIGHTCYAN_EX + link + Fore.RESET)
 
@@ -59,14 +79,26 @@ def download2():
     print (Fore.LIGHTMAGENTA_EX + 'Image name: ' + Fore.LIGHTCYAN_EX + image_name + Fore.RESET)
 
     resp = requests.get(link, stream=True)
-    local_file = open(image_name, 'wb')
-    resp.raw.decode_content = True
-    shutil.copyfileobj(resp.raw, local_file)
-    del resp
-    # Dowload image file
-    for i in tqdm(range(100), unit=" bits", desc= "Getting File"):
-      print('', end='\r')
-# ------------------------------------ /Loop as guest mode --------------------------------------#
+    try:
+        local_file = open(rute + tag + '/' + image_name, 'xb')
+        resp.raw.decode_content = True
+        shutil.copyfileobj(resp.raw, local_file)
+        del resp
+        # Dowload image file
+        for i in tqdm(range(100), unit=" bits", desc= "Getting File"):
+            print('', end='\r')
+    except:
+        errors()
+# ------------------------------------ /Loop as guest mode -------------------------------------- #
+
+# --------- Errors --------- #
+n = 0
+def errors():
+  global n
+  global number
+  n = n + 1
+  print(Fore.LIGHTRED_EX + '\n REPEATED\n' + Fore.RESET)
+# --------- /Errors --------- #
 
 while True:
     print(Fore.LIGHTCYAN_EX + '[-] Enter a password for' + Fore.LIGHTMAGENTA_EX +' lewd mode ' + Fore.LIGHTCYAN_EX + 'or press "Enter" to enter as a' + Fore.LIGHTYELLOW_EX + ' Guest' + Fore.RESET)
@@ -74,11 +106,14 @@ while True:
     if passwd == 'meow':
         print(Fore.LIGHTGREEN_EX + '[-] Password correct!!')
         time.sleep(0.5)
-        print(Fore.LIGHTGREEN_EX + '[-] Loading List...' + Fore.RESET)
-        time.sleep(1)
+        lewdfolder()
+        print(Fore.LIGHTGREEN_EX + '[-] Loading List...\n' + Fore.RESET)
+        time.sleep(0.5)
         while True:
+            print('# --------------------------------------- #')
             for elem in mylist:
                 print('[·] '+ Fore.LIGHTBLACK_EX + elem + Fore.RESET)
+            print('# --------------------------------------- #\n')
             print(Fore.LIGHTCYAN_EX + '\n[-] Select tag of image library' +Fore.CYAN + ' (Enter = predeterminate = neko): ' + Fore.RESET)
             tag = input()
                 
@@ -96,7 +131,18 @@ while True:
     elif passwd == "":
         link = nekos.cat()
         print(Fore.LIGHTGREEN_EX + 'Logged as a Guest' + Fore.RESET)
+        tag = 'Guest'
         break
+    elif passwd == "nigger":
+        print(Fore.LIGHTGREEN_EX + """
+ ███╗░░░██╗██╗░██████╗░░██████╗░███████╗██████╗
+ ████╗░░██║██║██╔════╝░██╔════╝░██╔════╝██╔══██╗
+ ██╔██╗░██║██║██║░░███╗██║░░███╗█████╗░░██████╔╝
+ ██║╚██╗██║██║██║░░░██║██║░░░██║██╔══╝░░██╔══██╗
+ ██║░╚████║██║╚██████╔╝╚██████╔╝███████╗██║░░██║
+ ╚═╝░░╚═══╝╚═╝░╚═════╝░░╚═════╝░╚══════╝╚═╝░░╚═╝""")
+        time.sleep(3)
+        exit()
     else:
         print(Fore.LIGHTRED_EX + '[-] Error... Retrying...' + Fore.RESET)
         time.sleep(2)
@@ -114,34 +160,39 @@ print (Fore.LIGHTMAGENTA_EX + 'Image name: ' + Fore.LIGHTCYAN_EX + image_name + 
 # Open the url image, set stream to True, this will return the stream content.
 resp = requests.get(link, stream=True)
 
- # Open a local file with wb ( write binary ) permission.
-local_file = open(image_name, 'wb')
-
-# Set decode_content value to True, otherwise the downloaded image file's size will be zero.
-resp.raw.decode_content = True
-
 # Copy the response stream raw data to local image file.
-shutil.copyfileobj(resp.raw, local_file)
-# Remove the image url response object.
-del resp
+try:
+    # Open a local file with wb ( write binary ) permission.
+    local_file = open(rute + tag + '/' + image_name, 'xb')
+    # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
+    resp.raw.decode_content = True
+    shutil.copyfileobj(resp.raw, local_file)
+    # Remove the image url response object.
+    del resp
+    # Dowload image file
+    for i in tqdm(range(100), unit=" bits", desc= "Getting File"):
+        print('', end='\r')
+except:
+    errors()
 
-# animation
-for i in tqdm(range(100), unit=" bits", desc= "Getting File"):
-  print('', end='\r') 
+# ------------------------------------ Repeat -------------------------------------- #
+def repeat():
+    global number
+    if passwd == 'meow': 
+        for ejecucion in range(number - 1):
+            download()
+    elif passwd == "":
+        for ejecucion in range(number - 1):
+            download2()
+repeat()
+# ------------------------------------ /Repeat -------------------------------------- #
 
-# ------------------------------------ Repeat --------------------------------------#
-if passwd == 'meow': 
-    for ejecucion in range(number - 1):
-        download()
-elif passwd == "":
-    for ejecucion in range(number - 1):
-        download2()
-# ------------------------------------ /Repeat --------------------------------------#
 
-# Rute.
-rute = os.getcwd()
 
 # Close script
-print(Fore.LIGHTGREEN_EX + 'All the files downloaded succesfully at "' + rute + '\"' + Fore.RESET)
+total = number - n
+print(Fore.LIGHTGREEN_EX + str(total) + ' files downloaded succesfully at "' + rute + '\"' + Fore.RESET)
+print(Fore.LIGHTRED_EX + 'Files repeated: ' + Fore.LIGHTRED_EX + str(n) + Fore.RESET )
+close = input('\nPress enter to close...')
 print(Fore.LIGHTYELLOW_EX + 'Closing...' + Fore.RESET)
 time.sleep(2)
