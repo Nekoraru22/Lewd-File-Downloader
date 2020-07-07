f5f4941d1258373ebf3d5288566971e620554c9f
Neko <57575090+Nekoraru22@users.noreply.github.com>
2020-07-07 21:20:19 +0200

    Update image-searcher.py

--- image-searcher.py
+++ image-searcher.py
@@ -17,16 +17,21 @@ print(Fore.GREEN + ' #                                                 #')
 print(Fore.GREEN + ' ###################################################' + Fore.RESET + '\n')
 
 # ------------------------------------ Lists -------------------------------------- #
-mylist = ['tickle', 'classic', 'ngif', 'erofeet', 'erok', 'poke', 'les', 'hololewd', 'lewdk', 'keta', 'feetg', 'nsfw_neko_gif', 'eroyuri', 'kiss', 'kuni', 'tits', 'pussy_jpg', 'cum_jpg', 'pussy', 'lewdkemo', 'slap', 'lewd', 'cum', 'cuddle', 'spank', 'smallboobs', 'Random_hentai_gif', 'avatar', 'fox_girl', 'nsfw_avatar', 'hug', 'gecg', 'boobs', 'pat', 'feet', 'smug', 'kemonomimi', 'solog', 'holo', 'wallpaper', 'bj', 'woof', 'yuri', 'trap', 'anal', 'baka', 'blowjob', 'holoero', 'feed', 'neko', 'gasm', 'hentai', 'futanari', 'ero', 'solo', 'waifu', 'pwankg', 'eron', 'erokemo']
+mylist = ['tickle', 'classic', 'ngif', 'erofeet', 'erok', 'poke', 'les', 'hololewd', 'lewdk', 'keta', 'feetg', 'nsfw_neko_gif', 'eroyuri', 'kiss', 'kuni', 'tits', 'pussy_jpg', 'cum_jpg', 'pussy', 'lewdkemo', 'slap', 'lewd', 'cum', 'cuddle', 'spank', 'smallboobs', 'Random_hentai_gif', 'avatar', 'fox_girl', 'nsfw_avatar', 'hug', 'gecg', 'boobs', 'pat', 'feet', 'smug', 'kemonomimi', 'solog', 'holo', 'wallpaper', 'bj', 'woof', 'yuri', 'trap', 'anal', 'baka', 'blowjob', 'holoero', 'feed', 'neko', 'gasm', 'hentai', 'futanari', 'ero', 'solo', 'waifu', 'pwankg', 'eron', 'erokemo'] # Api nekos.life
+mylist2 = ['neko', 'futa', 'kawaii', 'lewd', 'slave', 'pat', 'monster'] # Api lolis.life
 mylist.sort()
 # ------------------------------------ /Lists -------------------------------------- #
 
 # ------------------------------------ Create folders -------------------------------------- #
-os.makedirs('Downloader/Guest', mode=0o777, exist_ok=True)
+os.makedirs('Downloader/nekos_life/Guest', mode=0o777, exist_ok=True)
 
 def lewdfolder():
     for names in mylist:
-        os.makedirs('Downloader/' + names, mode=0o777, exist_ok=True)
+        os.makedirs('Downloader/nekos_life/' + names, mode=0o777, exist_ok=True)
+    
+    for names in mylist2:
+        os.makedirs('Downloader/lolis_life/' + names, mode=0o777, exist_ok=True)
+
     print(Fore.LIGHTGREEN_EX + '[-] Folders created succesfully' + Fore.RESET)
 # ------------------------------------ /Create folders -------------------------------------- #
 
@@ -43,20 +48,43 @@ rute = 'Downloader/'
 
 # ------------------------------------ Loop as lewd mode -------------------------------------- #
 def download():
-    global tag
-    global rute
-    link = nekos.img(tag)
+    global tag, rute, foldername, link, apianswer
+    
+    if apianswer == '1':
+        link = nekos.img(tag)
+
+    elif apianswer == '2':
+
+        if tag == "random":
+            r = requests.get("https://api.lolis.life/random")
+            link = BeautifulSoup(r.content,"html.parser")
+            link = re.sub(r"[^a-zA-Z0-9/.:\"]"," ",str(link))
+            link = re.search(r"url\":\"([a-zA-Z0-9-_./:]+)", link)
+            link = link.group(1)
+
+        else:
+            r = requests.get("https://api.lolis.life/" + tag)
+            link = BeautifulSoup(r.content,"html.parser")
+            link = re.sub(r"[^a-zA-Z0-9/.:\"]"," ",str(link))
+            link = re.search(r"url\":\"([a-zA-Z0-9-_./:]+)", link)
+            link = link.group(1)
+    
     print(Fore.LIGHTMAGENTA_EX + "URL Image: " + Fore.LIGHTCYAN_EX + link + Fore.RESET)
 
-    image_name = re.search(r"life/(.+[a-zA-Z0-9-_])", link)
-    image_name = image_name.group(1)
-    image_name = remove_char(image_name)
+    if apianswer == '1':
+        image_name = re.search(r"life/(.+[a-zA-Z0-9-_])", link)
+        image_name = image_name.group(1)
+        image_name = remove_char(image_name)
+    elif apianswer == '2':
+        image_name = re.search(r"life/([a-zA-Z0-9-_.]+)", link)
+        image_name = image_name.group(1)
+
     print (Fore.LIGHTMAGENTA_EX + 'Image name: ' + Fore.LIGHTCYAN_EX + image_name + Fore.RESET)
 
     resp = requests.get(link, stream=True)
     try:
         # Dowload image file
-        local_file = open(rute + tag + '/' + image_name, 'xb')
+        local_file = open(rute + foldername + '/' + image_name, 'xb')
         resp.raw.decode_content = True
         shutil.copyfileobj(resp.raw, local_file)
         del resp
@@ -69,8 +97,7 @@ def download():
 
 # ------------------------------------ Loop as guest mode -------------------------------------- #
 def download2():
-    global tag
-    global rute
+    global tag, rute, foldername
     link = nekos.cat()
     print(Fore.LIGHTMAGENTA_EX + "URL Image: " + Fore.LIGHTCYAN_EX + link + Fore.RESET)
 
@@ -82,7 +109,7 @@ def download2():
     resp = requests.get(link, stream=True)
     try:
         # Dowload image file
-        local_file = open(rute + tag + '/' + image_name, 'xb')
+        local_file = open(rute + foldername + '/' + image_name, 'xb')
         resp.raw.decode_content = True
         shutil.copyfileobj(resp.raw, local_file)
         del resp
@@ -92,6 +119,67 @@ def download2():
     except:
         errors()
 # ------------------------------------ /Loop as guest mode -------------------------------------- #
+
+# ------------------------------------ Nekos.Life -------------------------------------- #
+def nekoslife():
+    global link, tag, foldername
+    while True:
+        print(Fore.BLUE + '# --------------------------------------- #' + Fore.RESET)
+        for elem in mylist:
+            print('[·] '+ Fore.LIGHTBLACK_EX + elem + Fore.RESET)
+        print(Fore.BLUE + '# --------------------------------------- #\n' + Fore.RESET)
+        print(Fore.LIGHTCYAN_EX + '\n[-] Select tag of image library' +Fore.CYAN + ' (Enter = predeterminate(neko)): ' + Fore.RESET)
+        tag = input('\n==> ')
+        foldername = 'nekos_life/' + tag
+            
+        if tag in mylist:
+            link = nekos.img(tag)
+            break
+        elif tag == "":
+            link = nekos.img('neko')
+            tag = 'neko'
+            foldername = 'nekos_life/neko'
+            break
+        else:
+            print(Fore.LIGHTRED_EX +'[-] The tag not exists. Select another: ' + Fore.RESET)
+            time.sleep(2)
+# ------------------------------------ /Nekos.Life -------------------------------------- #
+
+# ------------------------------------ Lolis.Life -------------------------------------- #
+def lolislife():
+    global link, tag, foldername
+    while True:
+        print(Fore.BLUE + '# --------------------------------------- #' + Fore.RESET)
+        for elem in mylist2:
+            print('[·] '+ Fore.LIGHTBLACK_EX + elem + Fore.RESET)
+        print(Fore.BLUE + '# --------------------------------------- #\n' + Fore.RESET)
+        print(Fore.LIGHTCYAN_EX + '\n[-] Select tag of image library' + Fore.CYAN + ' (Enter = predeterminate(random)): ' + Fore.RESET)
+        tag = input('\n==> ')
+        foldername = 'lolis_life/' + tag
+            
+        if tag in mylist2:
+            r = requests.get("https://api.lolis.life/" + tag)
+            link = BeautifulSoup(r.content,"html.parser")
+            link = re.sub(r"[^a-zA-Z0-9/.:\"]"," ",str(link))
+            link = re.search(r"url\":\"([a-zA-Z0-9-_./:]+)", link)
+            link = link.group(1)
+            break
+        elif tag == "":
+            os.makedirs('Downloader/lolis_life/random', mode=0o777, exist_ok=True)
+
+            r = requests.get("https://api.lolis.life/random")
+            link = BeautifulSoup(r.content,"html.parser")
+            link = re.sub(r"[^a-zA-Z0-9/.:\"]"," ",str(link))
+            link = re.search(r"url\":\"([a-zA-Z0-9-_./:]+)", link)
+            link = link.group(1)
+
+            tag = 'random'
+            foldername = 'lolis_life/random'
+            break
+        else:
+            print(Fore.LIGHTRED_EX +'[-] The tag not exists. Select another: ' + Fore.RESET)
+            time.sleep(2)
+# ------------------------------------ /Lolis.Life -------------------------------------- #
 
 # --------- Errors --------- #
 n = 0
@@ -105,48 +193,53 @@ def errors():
 while True:
     print(Fore.LIGHTCYAN_EX + '[-] Enter a password for' + Fore.LIGHTMAGENTA_EX +' lewd mode ' + Fore.LIGHTCYAN_EX + 'or press "Enter" to enter as a' + Fore.LIGHTYELLOW_EX + ' Guest' + Fore.RESET)
     passwd = input('\n==> ')
+
     if passwd == 'meow':
         print(Fore.LIGHTGREEN_EX + '[-] Password correct!!')
         time.sleep(0.5)
         lewdfolder()
+
+        print(Fore.LIGHTCYAN_EX + "\n[-] what API do you want to use?" + Fore.LIGHTWHITE_EX + "\n   1) Nekos.life\n   2) Lolis.life")
+        apianswer = input('\n==> ')
         print(Fore.LIGHTYELLOW_EX + '[-] Loading List...\n' + Fore.RESET)
         time.sleep(0.5)
-        while True:
-            print(Fore.BLUE + '# --------------------------------------- #' + Fore.RESET)
-            for elem in mylist:
-                print('[·] '+ Fore.LIGHTBLACK_EX + elem + Fore.RESET)
-            print(Fore.BLUE + '# --------------------------------------- #\n' + Fore.RESET)
-            print(Fore.LIGHTCYAN_EX + '\n[-] Select tag of image library' +Fore.CYAN + ' (Enter = predeterminate = neko): ' + Fore.RESET)
-            tag = input()
-                
-            if tag in mylist:
-                link = nekos.img(tag)
-                break
-            elif tag == "":
-                link = nekos.img('neko')
-                tag = 'neko'
-                break
-            else:
-                print(Fore.LIGHTRED_EX +'[-] The tag not exists. Select another: ' + Fore.RESET)
-                time.sleep(2) 
-        break   
+
+        if apianswer == '1':
+            nekoslife()
+            break
+        elif apianswer == '2':
+            lolislife()
+            break
+        else:
+            print("That's not an option")
+
     elif passwd == "":
+        apianswer = "1"
         link = nekos.cat()
         print(Fore.LIGHTGREEN_EX + 'Logged as a Guest' + Fore.RESET)
         tag = 'Guest'
+        foldername = 'nekos_life/' + tag
         break
+        
     else:
         print(Fore.LIGHTRED_EX + '[-] Error... Retrying...' + Fore.RESET)
         time.sleep(2)
 
 number = int(input('\nEnter the number of files to download ==> '))
+
 print(Fore.BLUE + '\n# --- ' + '1' + ' --- #' + Fore.RESET)
 print(Fore.LIGHTMAGENTA_EX + "URL Image: " + Fore.LIGHTCYAN_EX + link + Fore.RESET)
 
 # Name of image.
-image_name = re.search(r"life/(.+[a-zA-Z0-9-_])", link)
-image_name = image_name.group(1)
-image_name = remove_char(image_name)
+if apianswer == '1':
+    image_name = re.search(r"life/(.+[a-zA-Z0-9-_])", link)
+    image_name = image_name.group(1)
+    image_name = remove_char(image_name)
+elif apianswer == '2':
+    image_name = re.search(r"life/([a-zA-Z0-9-_.]+)", link)
+    image_name = image_name.group(1)
+
+
 print (Fore.LIGHTMAGENTA_EX + 'Image name: ' + Fore.LIGHTCYAN_EX + image_name + Fore.RESET)
 
 # Open the url image, set stream to True, this will return the stream content.
@@ -155,7 +248,7 @@ resp = requests.get(link, stream=True)
 # Copy the response stream raw data to local image file.
 try:
     # Open a local file with wb ( write binary ) permission.
-    local_file = open(rute + tag + '/' + image_name, 'xb')
+    local_file = open(rute + foldername + '/' + image_name, 'xb')
     # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
     resp.raw.decode_content = True
     shutil.copyfileobj(resp.raw, local_file)
@@ -188,8 +281,8 @@ repeat()
 systemrute = os.getcwd()
 systemrute = systemrute.replace("\\", "/")
 total = number - n
-print(Fore.LIGHTGREEN_EX + str(total) + ' files downloaded succesfully at "' + systemrute + '/' + rute + tag + '\"' + Fore.RESET)
+print(Fore.LIGHTGREEN_EX + "\n" + str(total) + ' files downloaded succesfully at "' + systemrute + '/' + rute + foldername + '\"' + Fore.RESET)
 print(Fore.LIGHTRED_EX + 'Files repeated: ' + Fore.LIGHTRED_EX + str(n) + Fore.RESET )
 close = input('\nPress enter to close...')
 print(Fore.LIGHTYELLOW_EX + 'Closing...' + Fore.RESET)
-time.sleep(2)
+time.sleep(0.5)
